349b850fe0f49b627f9105ef7c69695a2e217f86
glory20h <56900470+glory20h@users.noreply.github.com>
2020-12-02 19:47:39 +0900

    12/2 7개 영상 뽑음

--- Assets/LEESET/Scripts/CustomInput.cs
+++ Assets/LEESET/Scripts/CustomInput.cs
@@ -20,9 +20,32 @@ public class CustomInput : MonoBehaviour
     {
         if(axisName == "Forklift_Ver")  //Forklift move forward
         {
-            if(timer < 12f)
+            if(timer < 7f)
+            {
+                return -0.15f;
+            }
+            else if(timer < 20f)
+            {
+                return -0.2f;
+            }
+            else
+            {
+                return Input.GetAxis(axisName);
+            }
+        }
+        else if(axisName == "Forklift_Hor")
+        {
+            if(timer > 3f && timer < 4f)
+            {
+                return 0.3f;
+            }
+            else if(timer > 5f && timer < 7f)
             {
                 return -0.3f;
+            }
+            else if(timer > 12f && timer < 14f)
+            {
+                return 0.15f;
             }
             else
             {
@@ -31,20 +54,32 @@ public class CustomInput : MonoBehaviour
         }
         else if(axisName == "Truck_Ver")    //Truck move forward
         {
-            if(timer > 12f)
+            if(timer > 9f)
             {
-                return -1f;
+                return -0.1f;
             }
             else
             {
                 return 0f;
             }
         }
+        else if(axisName == "Truck_Hor")
+        {
+            return Input.GetAxis(axisName);
+            if (timer > 14f)
+            {
+                return 0.3f;
+            }
+            else
+            {
+                return Input.GetAxis(axisName);
+            }
+        }
         else if(axisName == "Horizontal")   //Crane move horizontally
         {
-            if(timer > 1.5f && timer < 2f)
+            if(timer > 1f && timer < 3.36f)
             {
-                return -0.25f;
+                return 1f;
             }
             else
             {
@@ -53,9 +88,9 @@ public class CustomInput : MonoBehaviour
         }
         else if(axisName == "Vertical")     //Crane move vertically
         {
-            if (timer > 1f && timer < 4.2f)
+            if (timer > 0.4f && timer < 2.64f)
             {
-                return 0.75f;
+                return 1f;
             }
             else
             {
@@ -64,9 +99,9 @@ public class CustomInput : MonoBehaviour
         }
         else if(axisName == "Vertical_hook")    //Lower/Higher Crane
         {
-            if (timer > 0.75f && timer < 6f)
+            if (timer > 0.5f && timer < 5f)
             {
-                return 0.85f;
+                return 1f;
             }
             else
             {
